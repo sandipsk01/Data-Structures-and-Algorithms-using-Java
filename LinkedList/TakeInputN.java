ffcab56cb8c61c6eaf2753c80b78cb86d169a158
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-01-24 18:31:25 +0530

    Add files via upload

---
+++ LinkedList/TakeInputN.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class TakeInputN {
+	public static Node<Integer> takeInput(){
+        Scanner s= new Scanner(System.in);
+        int data = s.nextInt();
+        Node<Integer> head = null, tail=null;
+        
+        while(data != -1){
+          Node<Integer> currentNode= new Node<Integer>(data);
+	          if(head==null){
+	            head=currentNode;
+	            tail=currentNode;
+	          }else{
+	            tail.next= currentNode;
+	            tail=currentNode;
+	          }
+          data=s.nextInt();
+        }
+        return head;
+	}
+	
+	public static void main(String[] args){
+
+	      Node<Integer> head = takeInput();
+	  }
+}
